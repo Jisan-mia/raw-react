692918946dcecb041129522b10fca2b4ab9f381f
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-08-09 17:18:22 +0600

    a react app setup without cra

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+      <h1>React App without cra(create-react-app)</h1>
+    </>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.bundle.js",
+  },
+  // change webpack server port
+  devServer: {
+    port: '8000'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+};
