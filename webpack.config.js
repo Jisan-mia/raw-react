626be8ed52d581fd48e8d6e68a01220e8aff5ff7
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-08-11 17:30:28 +0600

    comment added on webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  entry: "./index.js",
-  output: {
+  mode: "development", // development or production
+  entry: "./index.js", // a main file which imports and have links with other files need to bundle
+  output: { // where the bundle file will be and which file(filename)
     path: path.resolve(__dirname, "dist"),
     filename: "index.bundle.js",
   },
@@ -12,12 +12,12 @@ module.exports = {
   devServer: {
     port: '8000'
   },
-  module: {
+  module: { // we need to tell webpack to use babel to transpile the code
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
+        test: /\.(js|jsx)$/, // look for all files that ends with .js or .jsx and bundle those file
+        exclude: /node_modules/, // don't bundle files inside node_modules
+        use: { // what is the loader i want to use?
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
@@ -27,7 +27,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({ // this html-webpack-plugin will take the index.html as a template and it will add to it a script tag with the bundled source file
       template: path.join(__dirname, "public", "index.html"),
     }),
   ],
